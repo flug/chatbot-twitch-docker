f6b6453d9bdfc665a7ca1aa95b7171b416b83554
Flug <flugv1@gmail.com>
2020-07-18 23:22:10 +0200

    cleaning code javascript and improvement of interaction ui

---
+++ client/config/production.js
@@ -0,0 +1,4 @@
+export default {
+    MERCURE_URL: "https://localhost:1337/.well-known/mercure",
+    MERCURE_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyJmb28iLCJiYXIiXSwicHVibGlzaCI6WyJmb28iXX19.afLx2f2ut3YgNVFStCx95Zm_UND1mZJ69OenXaDuZL8"
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,41 +1,70 @@
 import {EventSourcePolyfill, NativeEventSource} from 'event-source-polyfill';
+import setting from './config'
 
 (function (document) {
     const EventSource = NativeEventSource || EventSourcePolyfill;
-
     const audio = document.createElement('audio');
-    audio.controls = false;
-    audio.autoplay = true;
-
     const image = document.createElement('img');
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyJmb28iLCJiYXIiXSwicHVibGlzaCI6WyJmb28iXX19.afLx2f2ut3YgNVFStCx95Zm_UND1mZJ69OenXaDuZL8';
+    const container = document.getElementById('container');
+    const DEFAULT_TIMEOUT_FOR_CLEANING_CONTAINER = 10000;
+    const token = setting.MERCURE_TOKEN;
 
 
-    const url = new URL('https://localhost:1337/.well-known/mercure');
+    audio.addEventListener('createAudio', (e) => {
+        audio.src = e.detail.path;
+        audio.controls = false;
+        audio.autoplay = true;
+        container.append(audio)
+    })
+
+    image.addEventListener('createImage', (e) => {
+        image.src = e.detail.path
+        container.append(image)
+    })
+    container.addEventListener('clear', (e) => {
+        let duration = e.detail.duration;
+        container.classList.add('transition');
+        setTimeout((e) => {
+            container.classList.remove('transition');
+            container.innerHTML = '';
+            audio.pause()
+        }, duration || DEFAULT_TIMEOUT_FOR_CLEANING_CONTAINER)
+    })
+
+
+    const url = new URL(setting.MERCURE_URL);
     url.searchParams.append('topic', '/interact');
 
     let event = new EventSource(url, {authorizationHeader: "Bearer " + token});
 
-
     event.onmessage = (e) => {
 
         let data = JSON.parse(e.data)
-        audio.src = data.audio.path;
-        image.src = data.image.path;
+        console.log(data.audio.path);
+        if (data.hasOwnProperty('audio')) {
+            let ev = new CustomEvent("createAudio", {
+                detail: {
+                    path: data.audio.path,
+                }
+            });
+            audio.dispatchEvent(ev);
+        }
+        if (data.hasOwnProperty('image')) {
+            let ev = new CustomEvent("createImage", {
+                detail: {
+                    path: data.image.path,
+                }
+            });
+            image.dispatchEvent(ev);
+        }
 
-
-        document.getElementById('container').innerHTML = audio.outerHTML;
-        document.getElementById('container').innerHTML += image.outerHTML;
-
-        setTimeout(() => {
-            document.getElementById('container').classList.add('transition');
-            document.getElementById('container').innerText = '';
-            audio.pause();
-        }, data.duration)
-
+        container.dispatchEvent(new CustomEvent('clear', {
+            detail: {
+                duration: data.duration.time
+            }
+        }))
     }
 
-    console.log('ok');
 })(document);
 
 
